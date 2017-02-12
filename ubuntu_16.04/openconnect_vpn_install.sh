9ff59d747bbe5073ba29d5d325b9ab9ff71a2ffc
Michael Folkerts <mmfolkerts@gmail.com>
2017-02-12 14:25:27 -0600

    added vpnc script location reference

--- ubuntu_16.04/openconnect_vpn_install.sh
+++ ubuntu_16.04/openconnect_vpn_install.sh
@@ -2,13 +2,14 @@
 
 echo "installing openconnect from source"
 
-#[ref](https://github.com/dnschneid/crouton/wiki/Juniper-Junos-Pulse-VPN-with-OpenConnect)
+# [ref](https://github.com/dnschneid/crouton/wiki/Juniper-Junos-Pulse-VPN-with-OpenConnect)
 
 sudo apt-get install vpnc git autoconf libtool gettext libssl-dev libxml2-dev build-essential
 git clone git://git.infradead.org/users/dwmw2/openconnect.git
 
 cd openconnect
 autoreconf --install --verbose
+# [ref](http://packages.ubuntu.com/xenial/amd64/vpnc-scripts/filelist)
 ./configure --with-vpnc-script=/usr/share/vpnc-scripts/vpnc-script --without-openssl-version-check
 make
 sudo make install
